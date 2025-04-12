7ac0495da4bd877afaa00aaafef2aa8d9968ab2b
Kevin Forte <kevinforte9875@gmail.com>
2025-04-12 11:15:08 +0200

    Bounce Animation Added

--- front-end/src/pages/MoviePage.jsx
+++ front-end/src/pages/MoviePage.jsx
@@ -77,7 +77,13 @@ function MoviePage() {
                     </div>
                 </div>
             ) : (
-                <p className="text-white">Caricamento...</p>
+                <li className="col-span-full flex justify-center items-center h-full">
+                    <div className="flex space-x-2">
+                        <span className="w-3 h-3 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></span>
+                        <span className="w-3 h-3 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></span>
+                        <span className="w-3 h-3 bg-white rounded-full animate-bounce"></span>
+                    </div>
+                </li>
             )}
 
             <div className="bg-black/70 p-4">
