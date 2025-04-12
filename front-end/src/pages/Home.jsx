9514ddaef399f0fe2eafd5c5504152e1a6f84e62
Kevin Forte <kevinforte9875@gmail.com>
2025-04-12 11:11:59 +0200

    Bounce Animation Added

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -27,9 +27,13 @@ function Home() {
                         </li>
                     ))
                 ) : (
-                    <p className="text-center text-lg">
-                        Caricamento in corso...
-                    </p>
+                    <li className="col-span-full flex justify-center items-center h-full">
+                        <div className="flex space-x-2">
+                            <span className="w-3 h-3 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></span>
+                            <span className="w-3 h-3 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></span>
+                            <span className="w-3 h-3 bg-white rounded-full animate-bounce"></span>
+                        </div>
+                    </li>
                 )}
             </ul>
 
