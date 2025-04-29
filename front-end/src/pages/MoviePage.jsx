a6b9573359aec3876ffee4415f48b65b8c3fa5a0
Kevin Forte <kevinforte9875@gmail.com>
2025-04-29 13:04:43 +0200

    Streaming Providers Added

--- front-end/src/pages/MoviePage.jsx
+++ front-end/src/pages/MoviePage.jsx
@@ -7,6 +7,8 @@ function MoviePage() {
     const [movie, setMovie] = useState(null);
     const [cast, setCast] = useState([]);
     const [trailerKey, setTrailerKey] = useState(null);
+    const [provider, setProvider] = useState([]);
+    const COUNTRY_CODE = "IT";
 
     const fetchYouTubeTrailer = async (movieId) => {
         const res = await fetch(
@@ -55,9 +57,28 @@ function MoviePage() {
             }
         };
 
+        const fetchProvider = async () => {
+            try {
+                const res = await fetch(
+                    `https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=${API_KEY}`
+                );
+                const data = await res.json();
+                console.log(data);
+                const countryData = data.results[COUNTRY_CODE];
+                if (countryData && countryData.flatrate) {
+                    setProvider(countryData.flatrate);
+                } else {
+                    setProvider([]);
+                }
+            } catch (error) {
+                console.error("Errore nel recupero dei provider:", error);
+            }
+        };
+
         fetchMovie();
         fetchCast();
         fetchTrailer();
+        fetchProvider();
     }, [id]);
 
     return (
@@ -71,11 +92,14 @@ function MoviePage() {
         >
             {movie ? (
                 <div className="lg:gap-10 md:p-2 min-h-screen flex bg-black/70 flex-col md:flex-row justify-center items-center">
-                    <a href="#trailer">
+                    <a
+                        href="#trailer"
+                        className="cursor-pointer mt-4 md:mt-0 border-1 border-gray-600 rounded-xl"
+                    >
                         <img
                             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                             alt={`${movie.title} IMG`}
-                            className="mt-4 md:mt-0 cursor-pointer border-2 border-transparent hover:scale-105 hover:border-yellow-400 transition max-h-84 md:max-h-96 lg:max-h-[420px] rounded-xl"
+                            className="border-2 border-transparent hover:scale-105 hover:border-yellow-400 transition max-h-84 md:max-h-96 lg:max-h-[420px] rounded-xl"
                         />
                     </a>
                     <div className="text-white mt-4 p-4">
@@ -96,6 +120,34 @@ function MoviePage() {
                             <span>⭐{movie.vote_average.toFixed(1)}/10</span>
                             <span>📅{movie.release_date}</span>
                             <span>🕖{movie.runtime}min</span>
+                        </div>
+                        <div className="my-2">
+                            <span className="text-2xl font-semibold">
+                                Available on:{" "}
+                            </span>
+                            {provider.length > 0 ? (
+                                <ul className="flex flex-col justify-center items-start">
+                                    {provider.map((provider) => (
+                                        <li
+                                            key={provider.provider_id}
+                                            className="flex flex-row gap-3 my-1 justify-center items-center"
+                                        >
+                                            <img
+                                                src={`https://image.tmdb.org/t/p/w45${provider.logo_path}`}
+                                                alt={provider.provider_name}
+                                                className="h-8 w-8 rounded-full"
+                                            />
+                                            <strong className="text-yellow-400">
+                                                {provider.provider_name}
+                                            </strong>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <p className="underline">
+                                    Not currently available in your country
+                                </p>
+                            )}
                         </div>
                         <p className="w-full text-md md:max-w-[75ch]">
                             {movie.overview}
