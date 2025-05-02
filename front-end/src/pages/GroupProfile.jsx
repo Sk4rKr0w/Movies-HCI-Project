d4e9ded2bb7e34e5984c1b319aacb75a693f3aa3
Kevin Forte <kevinforte9875@gmail.com>
2025-05-02 13:04:43 +0200

    Minor Fix

--- front-end/src/pages/GroupProfile.jsx
+++ front-end/src/pages/GroupProfile.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo, useRef } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import supabase from "../supabaseClient";
